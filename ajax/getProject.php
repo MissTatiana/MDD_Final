8bce241be72ccc4c4c3a80689864c68e75d754b3
Tatiana Kerick <KerickTatiana@gmail.com>
2014-05-03 01:52:18 -0400

    mobile all done, tablet a bit wonky, started to work on the projects functionality of things

---
+++ ajax/getProject.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+ * Tatiana Kerick
+ * PocketAD - Shot List Manager 
+ * Retreieve the list of projects from the data base
+*/
+
+ini_set( "display_errors", 1);
+error_reporting(-1);
+
+$db = new PDO("mysql:host=localhost; dbname=CameraMan", "root", "root");
+
+$query = "select * from projectList order by project_id";
+
+$st = $db->prepare($query);
+$st->execute();
+
+$projects = $st->fetchAll();
+
+//json encode the response
+echo $json_response = json_encode($projects);
+
+
+?>
